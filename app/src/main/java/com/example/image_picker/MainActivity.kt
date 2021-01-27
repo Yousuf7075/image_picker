99f521efbfd426e84f0455d51d6eda8d400d00ee
Yousuf <aarajyousuf.qtec@gmail.com>
2021-01-27 19:28:00 +0600

    design ui

--- app/src/main/java/com/example/image_picker/MainActivity.kt
+++ app/src/main/java/com/example/image_picker/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.image_picker
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.opensooq.supernova.gligar.GligarPicker
 
 class MainActivity : AppCompatActivity() {
+
+    val PICKER_REQUEST_CODE = 30
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GligarPicker().limit(10).disableCamera(false).cameraDirect(false).requestCode(PICKER_REQUEST_CODE)
+            .withActivity(this).show()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when (requestCode) {
+            PICKER_REQUEST_CODE -> {
+                val imagesList = data?.extras?.getStringArray(GligarPicker.IMAGES_RESULT)
+                if (!imagesList.isNullOrEmpty()) {
+                   val imagesCount = "Number of selected Images: ${imagesList.size}"
+                    Log.e("img", imagesCount)
+                }
+            }
+        }
+    }
+
 }
